f90fe37b13593578ebec558fe9fb4a7e8bd4559d
Anton Levholm <anton.badminton@gmail.com>
2018-03-23 14:19:08 +0100

    Hello, world

---
+++ backend/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='SingStore List Manager',
+    version='0.1.4',
+    description='',
+    author='Anton Levholm',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['flask', 'requests', 'scrapy']
+)
